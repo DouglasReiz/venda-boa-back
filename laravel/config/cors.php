072bd1a34dd1b5e3edbf0f94249dfa223de807bd
Douglas Alves <douglasreisalves02@gmail.com>
2026-06-10 14:19:38 +0000

    Ajustes de PRD

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://venda-boa-pdv.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
